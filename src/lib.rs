3e5de6f3a64dec925813d1817c63c4e8b5d2970b
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2024-12-14 21:59:21 +0300

    Add mnemonic generator struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+use rand::Rng;
+
+pub struct MnemonicGenerator {}
+
+impl MnemonicGenerator {
+    pub fn generate() -> String {
+        let left = ["crazy", "amazing"];
+        let right = ["steve", "alan", "einstein"];
+
+        let left_idx = rand::thread_rng().gen_range(0..left.len());
+        let right_idx = rand::thread_rng().gen_range(0..right.len());
+
+        format!("{}_{}", &left[left_idx], &right[right_idx])
+    }
+}
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -7,8 +22,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn generate_two_words() {
+        let mnemonic = MnemonicGenerator::generate();
+        let parts: Vec<&str> = mnemonic.split('_').collect();
+        assert_eq!(parts.len(), 2);
     }
 }
